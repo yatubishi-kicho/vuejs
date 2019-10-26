8a36247964c1c79840b0f643d13f8f87e5c6a9f1
kicho <yatubishi.kicho@gmail.com>
2019-10-26 15:43:02 +0000

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,80 @@
+(function() {
+  'use strict';
+  
+    var likeComponent = Vue.extend({
+        props: {
+            message: {
+                type: String,
+                default: 'Like'
+            }
+        },
+        data: function(){
+            return{
+                count: 0
+            }
+        },
+        template: '<button @click="countup">{{ message }} {{ count }}</button>',
+        methods:{
+            countup: function(){
+                this.count++;
+                this.$emit('increment');
+            }
+        }
+    });
+  
+  var vm = new Vue({
+     el: '#app',
+     data: {
+         newItem: "",
+         todos: [],
+         total: 0
+     },
+     watch:{
+       todos:{ 
+        handler: function() {
+           localStorage.setItem('todos', JSON.stringify(this.todos));
+        },
+        deep: true
+       }  
+     },
+     mounted: function(){
+         this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+     },
+     methods:{
+         addItem: function(){
+             var item = {
+                 title: this.newItem,
+                 isDone: false
+             }
+             this.todos.push(item)
+             this.newItem='';
+         },
+         deleteItem: function(index){
+             if(confirm('are you sure?')){
+                 this.todos.splice(index,1);
+             }
+         },
+         purge: function(){
+             if(!confirm('delete finished?')){
+                 return;
+             }
+             this.todos = this.remaining;
+         },
+         incrementTotal: function(){
+             this.total++;
+         }
+     },
+     computed:{
+         remaining: function(){
+            return this.todos.filter(function(todo){
+                 return !todo.isDone;
+             });
+         }
+     },
+     components:{
+         'like-component': likeComponent
+     }
+  });
+  
+  
+})();
